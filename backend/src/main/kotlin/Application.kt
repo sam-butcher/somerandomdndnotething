0c0e15f46959f53aa7ba1ff46c1309077d6e0d8c
sam-butcher <samuel@typedb.com>
2026-01-08 10:32:57 +0000

    skeleton

---
+++ backend/src/main/kotlin/Application.kt
@@ -0,0 +1,12 @@
+package com.sambutcher
+
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureSecurity()
+    configureRouting()
+}
